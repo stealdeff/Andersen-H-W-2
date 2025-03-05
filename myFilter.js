a559e007505ba2d02d28e3208d18231c59721333
Arina <Ukam.naletko@mail.ru>
2025-03-05 11:37:53 +0300

    .myFilter

--- myFilter.js
+++ myFilter.js
@@ -1,18 +1,23 @@
-
-function myFilter(array, callback, contextObject) {
+const obj={
+    array:[],
+    callback:null,
+    contextObject:null,
+     myFilter() {
     let result = [];
-    for (let index = 0; index < array.length; index++) {
-        if (array[index] !== undefined) {
-            if (callback.call(contextObject, array[index], index, array)) {
-                result.push(array[index]);
+    for (let index = 0; index < this.array.length; index++) {
+        if (this.array[index] !== undefined) {
+            if (this.callback.call(this.contextObject, this.array[index], index, this.array)) {
+                result.push(this.array[index]);
             }
         }
     }
 
     return result;
 }
-
+};
 const arr=[1,4,5,2,3,5,1,2]
-let res=myFilter(arr,e=>e<5);
+obj.array=arr
+obj.callback=(e)=>e<5
+obj.contextObject=null
+let res=obj.myFilter()
 console.log(res)
-
